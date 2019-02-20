ffbc6c6c162c193dfb9d100aea7c032f78553b63
paspheeris <pa.spheeris@gmail.com>
2019-02-20 11:08:05 -0800

    Switch out Browser.sandbox for Html.program, to get things running

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,32 @@
-import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Platform.Cmd
 
 main =
-  Browser.sandbox { init = 0, update = update, view = view }
+  Html.program
+      { init = (0, Cmd.none)
+      , update = update
+      , view = view
+      , subscriptions = \_ -> Sub.none
+      }
 
 type Msg = Increment | Decrement
 
+type alias Model = Int
+
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Increment ->
-      model + 1
+      ( model + 1 ) ! []
 
     Decrement ->
-      model - 1
+      ( model - 1 ) ! []
 
 view model =
   div []
     [ div [] [text "Basically Hacker News!"]
     , button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
+    , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
     ]
