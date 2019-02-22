e7d5ea32fbd2f323dba5253805e5c4237aa328ba
Nolan Brady <nolanrbrady@gmail.com>
2019-02-22 15:58:38 -0700

    Text input in and doing stuff kind of

--- src/Main.elm
+++ src/Main.elm
@@ -2,28 +2,33 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (..)
+import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 
 
 ---- MODEL ----
 
 
-type alias Model =
-    {}
+type alias Model = {
+    item : String,
+    list : List String }
+
+initModel : Model 
+initModel = {
+    item = "",
+    list = [] }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( initModel, Cmd.none )
 
 
 
 ---- UPDATE ----
-
-
 type Msg
     = NoOp
-
+    | SubmitItem
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
@@ -32,18 +37,22 @@ update msg model =
 
 
 ---- VIEW ----
-
-
 view : Model -> Html Msg
 view model =
     div []
         [ img [ src "../public/logo.svg" ] []
-        , h1 [ style "color" "red"] [ text "Attempting a To Do list!" ]
-        , p [] [ text "This is another test"]
-        , input [ style "width" "200px", style "border-radius" "15px"] []
-        , button [] [text "Submit Item"]
-        , div [] [ text "This is a test"
-        , div [ style "color" "blue"] [ text "Another One" ]]
+        , div [style "justify-content" "center", style "align-items" "center"] 
+                [ h1 [ style "color" "red"] [ text "Attempting a To Do list!" ]
+            , p [] [ text "This is another test"]
+            , input [ style "width" "200px"
+                    , style "border-radius" "15px"
+                    , value model.item
+                    , placeholder "List your item"] []
+            , div [ style "padding-top" "20px"] [ button [onClick SubmitItem] [text "Submit Item"]]
+            , div [] [ text "This is a test"
+            , div [ style "color" "blue"] [ text "Another One" ]
+            , div [ style "width" "100%", style "text-align" "center"] [ text "This is another div just for shits"]]
+            ]
         ]
 
 
